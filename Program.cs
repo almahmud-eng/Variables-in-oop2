3535e23d6b872f2d93cc8dbbf4f71b81d50dcb6d
almahmud-eng <almahmudaponofficial@gmail.com>
2026-04-29 22:59:06 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Let's Start Programming 
+
+            //variables
+
+            int x; //Declaration
+
+            x = 12; //Initialization
+
+            int y = 21; // Declaration + Initialisation;
+
+            int z = x + y; // summation 
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine("The Sum of X & Y is "+z);
+
+            int age = 23;
+            Console.WriteLine("Your Age is = "  +   age );
+            double height = 1233.69;
+            Console.WriteLine("Your Height is = " + height + "cm");
+            bool alive = true;
+            Console.WriteLine("Are you alive? " + alive);
+            char symbol = '$';
+            Console.WriteLine(symbol);
+            String name = "bro"; 
+            Console.WriteLine(name+symbol);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
